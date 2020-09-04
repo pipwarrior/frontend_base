2bfd55af0e6b9178c5bb2ffa19f345e8bc3c8d88
Yap Feng Wei <yapfengwei@MacBook-Pro.local>
2020-09-04 17:31:05 +0800

    Gerneralize isMobile function

--- src/Pages/Catalog/Catalog.js
+++ src/Pages/Catalog/Catalog.js
@@ -2,27 +2,9 @@ import React from 'react';
 import CatalogMaximise from './CatalogMaximise';
 import CatalogMinimise from './CatalogMinimise';
 
+import isMobile from '../../Utilities/isMobile';
+
 const Catalog = (props) => {
-  var isMobile = {
-    Android: function() {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
-    },
-    any: function() {
-        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-  };
 
   const selectedItem = props.location.selectedItem;
 
